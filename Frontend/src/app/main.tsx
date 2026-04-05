4a771ac4f04d55734f8e95e807c4bfb68446be1c
yannou64 <yannickpro64@gmail.com>
2026-04-05 17:15:30 +0200

    style: mise en place du style de l'application

--- Frontend/src/app/main.tsx
+++ Frontend/src/app/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './styles/global.scss';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <App />
+    </StrictMode>,
+);
